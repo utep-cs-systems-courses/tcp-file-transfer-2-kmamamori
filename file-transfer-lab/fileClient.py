421d69d3da8b3780c209a6c677d6c2f752635a62
kmamamori <kmamamori@miners.utep.edu>
2020-10-06 21:59:26 -0600

    Added code and files to 'master' branch.

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+import socket, sys, re
+sys.path.append("../lib")
+import params
+
+from frameSock import frameSend, framedReceive
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+addrFamily = socket.AF_INET
+socktype = socket.SOCK_STREAM
+addrPort = (serverHost, serverPort)
+
+s.socket.socket(addrFamily, socktype)
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+s.connect(addrPort)
+
+sendingFile = input("Enter the file name")
+if exists(sendingFile):
+    cp_file = open(sendingFile, 'r')
+    data_file = cp_file.read()
+    if len(data_file)==0:
+        print("Empty file")
+        sys.exit(0)
+    else:
+        framedSend(s, sendingFile.encode(), debug)
+        try:
+            fileExistance = framedReceive(s, debug)
+            fileExistance = fileExistance.decode()
+        except:
+            print("Connection has being disconnected")
+        if fileExistance == True:
+            print("File already exist in the server")
+            sys.exit(0)
+        else:
+            try:
+                framedSend(s, data_file, True)
+            except:
+                print("Error: check your conection status")
+            try:
+                framedReceive(s, True)
+            except:
+                print('Error: check your connection status')
+else:
+    print("file not found.")
+    sys.exit(0)
+    s.close()
---
+++ file-transfer-lab/fileServer.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+
+import sys, os, re
+sys.path.append("../lib")
+import params
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "echosever"
+paramMap = params.parseParams(switchesVArdefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket(socket.AF_INET, socket.SOCK_STREAM)
+bindAddr = ('127.0.0.1', listenPort)
+lsock.listen(5)
+print('listening on:', bindAddr)
+
+while True:
+    sock, addr = lsock.accept()
+    print("connection rec'd from", addr)
+    if not os.fork():
+        while Ture:
+            payload = framedReceive(sock, debug)
+            if not payload:
+                break
+            payload = payload.decode()
+
+            if exists(payload):
+                framedSend(sock, b"True", debug)
+            else:
+                frameSend(sock, b"False", True)
+                try:
+                    payload2 = frameReceive(sock, debug)
+                except:
+                    print("Connection has being disconnected.")
+                    sys.exit(1)
+                if not payload2:
+                    break
+                payload += b"!"
+                try:
+                    frameSend(sock, payload, debug)
+                except:
+                    print("Connection has being disconnected")
+                    sys.exit(1)
+                output = open(payload, 'wb')
+                output.write(payload2)
+                sock.close()
